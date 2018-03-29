1d9dbf4c39756b9e2fc3f37c0d69d62baddadfbc
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-29 23:27:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,13 +2,15 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
-  @@artists = []
-  @@genres = []
+
+
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
+    @@artists << artist
     @genre = genre
+    @@genres = << genre
     @@count += 1
     #@@genres << genre unless genres.include?(genre)
     #end
