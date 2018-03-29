5f6608ac12a4a613952cd95e95508517d504edf1
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-29 23:25:08 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
-    @@genres << genre unless genre.include?(genre)
+    @@genres << genre unless genres.include?(genre)
     #end
   end
 
